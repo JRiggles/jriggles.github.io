09c79f51b28aa92e7c66ae1b25f78ced06d8d803
John Riggles <jriggles@icloud.com>
2024-03-04 17:12:37 -0500

    add public API key for stackexchange reqs

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,19 @@
 import * as feather from 'feather-icons'
 
+const STACKEXAPIKEY = ")q0PGY5frwc43NqR*gt6hQ((";
+
 
 window.addEventListener('load', function() {
     feather.replace();
-    // getSoReputation();
-    // getSoTags(2);
-})
+    getSoReputation();
+    getSoTags(2);
+});
 
 
 function getSoReputation() {
     const apiUrl = 'https://api.stackexchange.com/2.3/users/8512262?site=stackoverflow';
 
-    fetch(apiUrl)
+    fetch(apiUrl + "&key=" + STACKEXAPIKEY)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -23,9 +25,7 @@ function getSoReputation() {
             if (data.items.length > 0) {
                 let repValue = data.items[0].reputation;
                 reputationText.innerText = repValue.toLocaleString();
-                repValue.classList.add('is-dark');
-            } else {
-                reputationText.innerText = '----';
+                reputationText.classList.add('is-dark');
             }
         })
         .catch(_error => {
@@ -40,7 +40,7 @@ function getSoTags(nTags) {
     const apiUrl = 'https://api.stackexchange.com/2.3/users/8512262/tags?site=stackoverflow';
     const soUserUrl = 'https://stackoverflow.com/search?q=user:8512262'
 
-    fetch(apiUrl)
+    fetch(apiUrl + "&key=" + STACKEXAPIKEY)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -48,13 +48,12 @@ function getSoTags(nTags) {
             return response.json();
         })
         .then(data => {
+            let placeholder = document.getElementById('tagPlaceholder');
             if (data.items.length > 0) {
                 // hide placeholder
-                let placeholder = document.getElementById('tagPlaceholder');
                 placeholder.style.display = 'none';
                 // load tags
                 let topTags = data.items.slice(0, nTags).map(tag => tag.name);
-                console.log(topTags);
                 let tagContainer = document.getElementById('topTags');
                 topTags.forEach(tag => {
                     let tagLink = document.createElement('a');
