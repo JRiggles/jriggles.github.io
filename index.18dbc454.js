7e8993780ad967a551fedf4ddf78e237a35ca0c3
John Riggles <jriggles@icloud.com>
2024-03-04 17:12:51 -0500

    Updates

--- index.18dbc454.js
+++ index.18dbc454.js
@@ -585,14 +585,15 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"1SICI":[function(require,module,exports) {
 var _featherIcons = require("feather-icons");
+const STACKEXAPIKEY = ")q0PGY5frwc43NqR*gt6hQ((";
 window.addEventListener("load", function() {
     _featherIcons.replace();
-// getSoReputation();
-// getSoTags(2);
+    getSoReputation();
+    getSoTags(2);
 });
 function getSoReputation() {
     const apiUrl = "https://api.stackexchange.com/2.3/users/8512262?site=stackoverflow";
-    fetch(apiUrl).then((response)=>{
+    fetch(apiUrl + "&key=" + STACKEXAPIKEY).then((response)=>{
         if (!response.ok) throw new Error("Network response was not ok");
         return response.json();
     }).then((data)=>{
@@ -600,8 +601,8 @@ function getSoReputation() {
         if (data.items.length > 0) {
             let repValue = data.items[0].reputation;
             reputationText.innerText = repValue.toLocaleString();
-            repValue.classList.add("is-dark");
-        } else reputationText.innerText = "----";
+            reputationText.classList.add("is-dark");
+        }
     }).catch((_error)=>{
         let reputationText = document.getElementById("reputation");
         reputationText.innerText = "error fetching reputation";
@@ -611,17 +612,16 @@ function getSoReputation() {
 function getSoTags(nTags) {
     const apiUrl = "https://api.stackexchange.com/2.3/users/8512262/tags?site=stackoverflow";
     const soUserUrl = "https://stackoverflow.com/search?q=user:8512262";
-    fetch(apiUrl).then((response)=>{
+    fetch(apiUrl + "&key=" + STACKEXAPIKEY).then((response)=>{
         if (!response.ok) throw new Error("Network response was not ok");
         return response.json();
     }).then((data)=>{
+        let placeholder = document.getElementById("tagPlaceholder");
         if (data.items.length > 0) {
             // hide placeholder
-            let placeholder = document.getElementById("tagPlaceholder");
             placeholder.style.display = "none";
             // load tags
             let topTags = data.items.slice(0, nTags).map((tag)=>tag.name);
-            console.log(topTags);
             let tagContainer = document.getElementById("topTags");
             topTags.forEach((tag)=>{
                 let tagLink = document.createElement("a");
