a5ace0a834ea783d2401ca6a787fab34de57285d
John Riggles <jriggles@icloud.com>
2024-03-07 22:20:48 -0500

    refactor placeholder loaders for fetched elements

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,27 @@ import * as feather from 'feather-icons'
 const STACKEXAPIURL = 'https://api.stackexchange.com/2.3/users/8512262'
 const STACKEXAPIKEY = ')q0PGY5frwc43NqR*gt6hQ((';  // public key
 
+var reputationTag = document.getElementById('reputationTag');
+var placeholderTag = document.getElementById('placeholderTag');
 
 window.addEventListener('load', function() {
     feather.replace();
+    showLoader(reputationTag);
+    showLoader(placeholderTag);
     getSoReputation();
     getSoTags(2);  // get top 2 stackoverflow tags
 });
 
 
+function showLoader(parentElement) {
+    parentElement.innerHTML = 'working...&nbsp;';
+    let loader = document.createElement('span');
+    loader.classList.add('has-loader');
+    parentElement.appendChild(loader);
+}
+
+
 function getSoReputation() {
-    let reputationText = document.getElementById('reputation');
     fetch(STACKEXAPIURL + "?site=stackoverflow&key=" + STACKEXAPIKEY)
         .then(response => {
             if (!response.ok) {
@@ -23,21 +34,20 @@ function getSoReputation() {
         .then(data => {
             if (data.items.length > 0) {
                 let repValue = data.items[0].reputation;
-                reputationText.innerText = repValue.toLocaleString();
-                reputationText.classList.add('is-dark');
+                reputationTag.innerText = repValue.toLocaleString();
+                reputationTag.classList.add('is-dark');
             }
         })
         .catch(error => {
             console.log(error);
-            reputationText.innerText = 'error';
-            reputationText.classList.add('is-danger');
+            reputationTag.innerText = 'error';
+            reputationTag.classList.add('is-danger');
         });
 }
 
 
 function getSoTags(nTags) {
     const soUserUrl = 'https://stackoverflow.com/search?q=user:8512262'
-    let placeholder = document.getElementById('tagPlaceholder');
     fetch(STACKEXAPIURL + "/tags?site=stackoverflow&key=" + STACKEXAPIKEY)
         .then(response => {
             if (!response.ok) {
@@ -48,11 +58,11 @@ function getSoTags(nTags) {
         .then(data => {
             if (data.items.length > 0) {
                 // hide placeholder
-                placeholder.style.display = 'none';
+                placeholderTag.style.display = 'none';
                 // load tags
-                let topTags = data.items.slice(0, nTags).map(tag => tag.name);
+                let topTagNames = data.items.slice(0, nTags).map(tag => tag.name);
                 let tagContainer = document.getElementById('topTags');
-                topTags.forEach(tag => {
+                topTagNames.forEach(tag => {
                     let tagLink = document.createElement('a');
                     tagLink.href = `${soUserUrl}+[${tag}]`;
                     tagLink.target = '_blank';
@@ -64,7 +74,7 @@ function getSoTags(nTags) {
         })
         .catch(error => {
             console.log(error);
-            placeholder.innerText = 'error';
-            placeholder.classList.add('is-danger');
+            placeholderTag.innerText = 'error';
+            placeholderTag.classList.add('is-danger');
         });
 }
