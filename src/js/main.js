31d2f708c5f6a0e9bf8dbefba6e30800c1f9c44b
John Riggles <jriggles@icloud.com>
2024-03-10 22:35:22 -0400

    refactor repetitive code into functions

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,60 @@ import * as feather from 'feather-icons'
 
 const STACKEXAPIURL = 'https://api.stackexchange.com/2.3/users/8512262'
 const STACKEXAPIKEY = ')q0PGY5frwc43NqR*gt6hQ((';  // public key
-
 var reputationTag = document.getElementById('reputationTag');
 var placeholderTag = document.getElementById('placeholderTag');
 
+showTagLoader(reputationTag);
+showTagLoader(placeholderTag);
+feather.replace();
+
+// DARK MODE
+// const body = document.querySelector('body');
+// const toggle = document.getElementById('darkModeToggle');
+
+// toggle.addEventListener("click", () => {
+//     const bodyCheck = body.classList.contains('dark');
+//     if (bodyCheck) {
+//         body.className = ''
+//     } else {
+//         body.className = "dark"
+//   }
+// })
+
+
 window.addEventListener('load', function() {
-    feather.replace();
-    showLoader(reputationTag);
-    showLoader(placeholderTag);
     getSoReputation();
     getSoTags(2);  // get top 2 stackoverflow tags
 });
 
 
-function showLoader(parentElement) {
-    parentElement.innerHTML = 'working...&nbsp;';
+function showTagLoader(tagElement) {
     let loader = document.createElement('span');
+    tagElement.innerHTML = 'working...&nbsp;';
     loader.classList.add('has-loader');
-    parentElement.appendChild(loader);
+    tagElement.appendChild(loader);
+}
+
+
+function handleTagResponse(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+}
+
+
+function handleTagError(tagElement, error) {
+    console.log(error);
+    tagElement.innerText = 'error';
+    tagElement.classList.add('is-danger');
 }
 
 
 function getSoReputation() {
     fetch(STACKEXAPIURL + "?site=stackoverflow&key=" + STACKEXAPIKEY)
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
+            return handleTagResponse(response);
         })
         .then(data => {
             if (data.items.length > 0) {
@@ -39,9 +65,7 @@ function getSoReputation() {
             }
         })
         .catch(error => {
-            console.log(error);
-            reputationTag.innerText = 'error';
-            reputationTag.classList.add('is-danger');
+            handleTagError(reputationTag, error);
         });
 }
 
@@ -50,10 +74,7 @@ function getSoTags(nTags) {
     const soUserUrl = 'https://stackoverflow.com/search?q=user:8512262'
     fetch(STACKEXAPIURL + "/tags?site=stackoverflow&key=" + STACKEXAPIKEY)
         .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
+            return handleTagResponse(response);
         })
         .then(data => {
             if (data.items.length > 0) {
@@ -67,14 +88,12 @@ function getSoTags(nTags) {
                     tagLink.href = `${soUserUrl}+[${tag}]`;
                     tagLink.target = '_blank';
                     tagLink.innerText = tag;
-                    tagLink.classList.add('tag', 'is-link');
+                    tagLink.classList.add('tag', 'is-info');
                     tagContainer.appendChild(tagLink);
                 });
             }
         })
         .catch(error => {
-            console.log(error);
-            placeholderTag.innerText = 'error';
-            placeholderTag.classList.add('is-danger');
+            handleTagError(placeholderTag, error);
         });
 }
